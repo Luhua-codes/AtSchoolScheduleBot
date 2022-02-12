b31bbb78f851b2508e048db8bcdb4958539c429e
Maria Sitkovets <m.sitkovets@gmail.com>
2022-02-12 18:33:03 -0500

    Created db and tables

---
+++ db.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=getpass("Enter password: "),
+        database="at_school_schedule",
+    ) as connection:
+        # create_db_query = "CREATE DATABASE IF NOT EXISTS at_school_schedule"
+        create_users_table_query = """
+            CREATE TABLE user(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                discord_user_id INT,
+                monday BOOLEAN DEFAULT false,
+                tuesday BOOLEAN DEFAULT false,
+                wednesday BOOLEAN DEFAULT false,
+                thursday BOOLEAN DEFAULT false,
+                friday BOOLEAN DEFAULT false,
+                mon_start_time_1 TIME DEFAULT NULL,
+                mon_end_time_1 TIME DEFAULT NULL,
+                mon_start_time_2 TIME DEFAULT NULL,
+                mon_end_time_2 TIME DEFAULT NULL,
+                mon_start_time_3 TIME DEFAULT NULL,
+                mon_end_time_3 TIME DEFAULT NULL,
+                tues_start_time_1 TIME DEFAULT NULL,
+                tues_end_time_1 TIME DEFAULT NULL,
+                tues_start_time_2 TIME DEFAULT NULL,
+                tues_end_time_2 TIME DEFAULT NULL,
+                tues_start_time_3 TIME DEFAULT NULL,
+                tues_end_time_3 TIME DEFAULT NULL,
+                wed_start_time_1 TIME DEFAULT NULL,
+                wed_end_time_1 TIME DEFAULT NULL,
+                wed_start_time_2 TIME DEFAULT NULL,
+                wed_end_time_2 TIME DEFAULT NULL,
+                wed_start_time_3 TIME DEFAULT NULL,
+                wed_end_time_3 TIME DEFAULT NULL,
+                thurs_start_time_1 TIME DEFAULT NULL,
+                thurs_end_time_1 TIME DEFAULT NULL,
+                thurs_start_time_2 TIME DEFAULT NULL,
+                thurs_end_time_2 TIME DEFAULT NULL,
+                thurs_start_time_3 TIME DEFAULT NULL,
+                thurs_end_time_3 TIME DEFAULT NULL,
+                fri_start_time_1 TIME DEFAULT NULL,
+                fri_end_time_1 TIME DEFAULT NULL,
+                fri_start_time_2 TIME DEFAULT NULL,
+                fri_end_time_2 TIME DEFAULT NULL,
+                fri_start_time_3 TIME DEFAULT NULL,
+                fri_end_time_3 TIME DEFAULT NULL
+            )
+            """
+        create_user_server_table_query = """
+        CREATE TABLE user_servers(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            discord_user_id INT,
+            discord_server_id INT
+        )
+        """
+        with connection.cursor() as cursor:
+            # cursor.execute(create_db_query)
+            cursor.execute(create_users_table_query)
+            cursor.execute(create_user_server_table_query)
+            connection.commit()
+except Error as e:
+    print(e)
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from enum import Enum
 import re
 
-
 class Weekday(Enum):
     MONDAY = 1
     TUESDAY = 2
@@ -19,7 +18,7 @@ intents = discord.Intents.default()
 intents.members = True
 
 # Get the client (bot) object from discord
-client = commands.Bot(command_prefix="sb! ", intents=intents)
+client = commands.Bot(command_prefix="sb!", intents=intents)
 
 
 # Add event listeners
@@ -50,7 +49,7 @@ async def ping(ctx):
 # DM the user
 @client.command()
 async def setup(ctx):
-    print(ctx.author)
+    print(ctx.author.id, ctx.author)
     message = "What days are you usually at school?"
     description = "Select by clicking the emotes of the weekdays. Click the check mark when you are done."
     embed = discord.Embed(title=message, description=description)
@@ -95,23 +94,9 @@ async def on_reaction_remove(reaction, user):
         await weekday_time(selected_weekdays, reaction.message.channel)
 
 
-# Helper function to ask a user what time they will be at school
+# Helper function to ask a user what time they will be at school and not in class
 async def weekday_time(weekdays, channel):
     for weekday in weekdays:
-        on_campus_message = "What time are you at school on " + weekday.lower().capitalize() + "?"
-        on_campus_description = "Enter the time you arrive and leave (example format: 0900 1600)"
-        on_campus_embed = discord.Embed(title=on_campus_message, description=on_campus_description)
-        await channel.send(embed=on_campus_embed)
-
-        def check_on_campus_time(msg):
-            # TODO: validate format
-            return True
-
-        user_on_campus_times = await client.wait_for("message", check=check_on_campus_time)
-        print(user_on_campus_times.content)
-        on_campus_times = user_on_campus_times.content.split(" ")
-        print(on_campus_times)
-
         available_message = "What times are you available on " + weekday.lower().capitalize() + "?"
         available_description = "Enter up to 3 time slots (example format: 0900 1200, 1400 1600)"
         available_embed = discord.Embed(title=available_message, description=available_description)
